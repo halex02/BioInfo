0f5caee2907cc2b45d1a61b1bfdf89b1e9873c02
halex02 <halex02@msn.com>
2016-02-03 23:19:56 +0100

    ajout des commentaires sur mutation.js (2)

--- BI-BURY-HOUPLON/lib/mutation.js
+++ BI-BURY-HOUPLON/lib/mutation.js
@@ -4,6 +4,7 @@ const random = require('./random.js') ;
 /*
  * randomMutation(i, path) : récupère le contenu d'un fichier fasta et fait subir à la chaine de nucléotide
  * autant de mutations que passées en paramètre. Renvoie un objet JSON.
+ * CU : le fichier ne doit contenir qu'une seule séquence.
  */
 var randomMutation= function (i, path) {
     var json_sequence =  parser.fastaFileToJsonObject(path) ;
@@ -42,6 +43,7 @@ var randomMutation= function (i, path) {
 }
 
     /*
+     * fonction qui imprime sur la console une séquence mutée.
      */
 exports.printMutation = function (i, path) {
     var json_mutated = randomMutation (i, path) ;
@@ -50,6 +52,9 @@ exports.printMutation = function (i, path) {
     console.log(json_mutated.sequences[0].sequence+'\n') ;
 }
 
+    /*
+     * fonction qui crée un fichier fasta dans path2 à partir d'un fichier fasta path1 dont la séquence a subi i mutations.
+     */
 exports.exportMutation = function (i, path1, path2) {
     json_mutated = randomMutation (i, path1) ;
     parser.jsonObjectToFastaFile(json_mutated) ;
