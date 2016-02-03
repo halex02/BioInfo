31f336ca90fe62506b6950ded9a8ebef868f6d98
halex02 <halex02@msn.com>
2016-02-03 23:15:33 +0100

    ajout des commentaires sur mutation.js (1)

--- BI-BURY-HOUPLON/lib/mutation.js
+++ BI-BURY-HOUPLON/lib/mutation.js
@@ -1,9 +1,18 @@
 const parser = require('./fastaParser.js') ;
 const random = require('./random.js') ;
 
+/*
+ * randomMutation(i, path) : récupère le contenu d'un fichier fasta et fait subir à la chaine de nucléotide
+ * autant de mutations que passées en paramètre. Renvoie un objet JSON.
+ */
 var randomMutation= function (i, path) {
     var json_sequence =  parser.fastaFileToJsonObject(path) ;
-    
+
+    /*
+     * fonction auxiliaire qui effectue une mutation sur la séquence donnée (string), prend également
+     * une fonction en paramètre pour définir la nature de la mutation: celle-ci prend 2 paramètres dont une chaine contenant les 4
+     * nucléotides.
+     */
     var muteUneFois = function (seq, func) {
 	var i = random.randomInt(0, seq.length-1) ;
 	
@@ -12,14 +21,19 @@ var randomMutation= function (i, path) {
 	return res ;
     } ;
 
-    var muteNFois = function (seq, i) {
+    /*
+     * fonction auxiliaire qui applique 'i' fois la mutation définie par la fonction 'func' sur la séquence 'seq'.
+     */
+    var muteNFois = function (seq, func, i) {
 	if(i == 1)
-	    return muteUneFois(seq, function(nucl, pattern){return pattern.replace(nucl, '')[random.randomInt(0, 2)];}) ;
+	    return muteUneFois(seq, func) ;
 	else
-	    return muteNFois(muteUneFois(seq, function(nucl, pattern){return pattern.replace(nucl, '')[random.randomInt(0, 2)];}), i-1) ;
+	    return muteNFois(muteUneFois(seq, func, i-1) ;
     } ;
 
-    var mutated_sequence = muteNFois(json_sequence.sequences[0].sequence, i) ;
+	var mutated_sequence = muteNFois(json_sequence.sequences[0].sequence,
+					 function(nucl, pattern){return pattern.replace(nucl, '')[random.randomInt(0, 2)];},
+					 i) ;
     
     var json_res = {sequences : [{id: json_sequence.sequences[0].id+'-mutated',
 				  sequence: mutated_sequence}]} ;
@@ -27,6 +41,8 @@ var randomMutation= function (i, path) {
     return json_res ;
 }
 
+    /*
+     */
 exports.printMutation = function (i, path) {
     var json_mutated = randomMutation (i, path) ;
 
