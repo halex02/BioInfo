fe4c31d55b74e706e4038ab4ec20b25479ac9209
halex02 <halex02@msn.com>
2016-02-03 15:51:19 +0100

    push mutation.js

--- BI-BURY-HOUPLON/lib/mutation.js
+++ BI-BURY-HOUPLON/lib/mutation.js
@@ -2,13 +2,22 @@ const parser = require('./fastaParser.js') ;
 const random = require('./random.js') ;
 
 exports.randomMutation(i, path) {
-    var json_sequences =  parser.fastaFileToJsonObject(path) ;
+    var json_sequence =  parser.fastaFileToJsonObject(path) ;
         
     var aux = function (seq, func) {
 	var i = random.randomInt(0, seq.length-1) ;
 	return seq.substring(0, i)+func(seq[i], 'ACTG')+substring(i+1, seq.length-1) ;
     } ;
 
-    var mutated_sequence = aux(json_sequences.sequences[0].sequence, (nucl, pattern)=> {return pattern.replace(nucl, '')[random.randomInt(0, 2)];})
-    
+    var aux2 = function (seq, i) {
+	if(i == 1)
+	    return aux(seq, (nucl, pattern)=> {return pattern.replace(nucl, '')[random.randomInt(0, 2)];}) ;
+	else
+	    return aux2(aux(seq, (nucl, pattern)=> {return pattern.replace(nucl, '')[random.randomInt(0, 2)];}), i-1) ;
+    } ;
+
+    var mutated_sequence = aux2(json_sequence.sequences[0].sequence, i) ;
+
+    return JSON.parse('{"sequences" : [{"id": "'+json_sequence.sequences[0].id+'-mutated",\n"sequence": "'+mutated_sequence+'"}]') ;
 }
+
