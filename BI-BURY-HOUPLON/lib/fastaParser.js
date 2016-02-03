dc6a4dd13bbffa855a93077c008215c88baa8ec4
halex02 <halex02@msn.com>
2016-02-03 22:41:26 +0100

    commentaire du fichier fastaParser.js

--- BI-BURY-HOUPLON/lib/fastaParser.js
+++ BI-BURY-HOUPLON/lib/fastaParser.js
@@ -1,17 +1,32 @@
+/*
+ * Utilisation de l'api FileSystem de Node.js pour gérer les entrée/sorties.
+ */
 const fs = require('fs');
 
+/*
+ * fastaStringToJsonString(str) : prend une chaine de caractère, str, représentant une séquence de nucléotide au format fasta
+ * et renvoie une chaine de caractère interprétable par l'objet JSON via sa méthode parse.
+ */
 exports.fastaStringToJsonString = function (str) {
+    //on isole la première ligne représentant l'id de la séquence en trouvant le premier retour-chariot et en faisant un substring...
     var del_index = str.indexOf('\n') ;
     var fasta_id = str.substring(0, del_index) ;
+    /*
+     *... et on fait la même chose avec la chaine de nucléotide, et on en profite pour supprimmer tous les retour-chariot qu'elle contient
+     *via une regex.
+     */
     var fasta_sequence = str.substr(del_index+1).replace(/\n/g,'') ;
+    //mise en forme de la chaine au format JSON pour la renvoyer ensuite.
     var json_string = '{"id": "'+fasta_id+'",\n"sequence": "'+fasta_sequence+'"}' ;
     return json_string ;
 }
 
-
-//à partir du path d'un fichier.fasta, le traduit en JSON, sous la forme d'un String, séparant chaque séquence.
+/*
+ * fastaFileToJsonString(path) : prend le chemin d'un fichier fasta , le lit, le traduit en chaîne de caractères convertible en objet JSON.
+ */
 exports.fastaFileToJsonString = function (path) {
-    var sequences_string = fs.readFileSync(path, 'ascii') ; 
+    //on récupère tout le contenu du fichier passé en paramètre, et on stocke chaque séquence dans une case d'un tableau.
+    var sequences_string = fs.readFileSync(path, 'ascii') ;
     var sequences_array = sequences_string.split('>') ;
     
     /* Comme on utilise le '>' comme délimiteur pour éclater la chaine
@@ -20,6 +35,11 @@ exports.fastaFileToJsonString = function (path) {
      */
     sequences_array.shift() ;
 
+    /*
+     * fonction auxiliaire, récursive terminale, qui va parser chaque chaine du fichier fasta, stockée dans le tableau
+     * passé en paramètre, utilisé comme une pile, et la stocker dans la chaine str qui sert d'accumulateur pour la fonction,
+     * avant de la renvoyer après avoir dépilé et parser la dernière séquence d'adn.
+     */
     var aux = function (arr, str) {
 	if (arr.length == 1)
 	    return '{"sequences": ['+str+exports.fastaStringToJsonString(arr.shift())+']}' ;
@@ -32,12 +52,17 @@ exports.fastaFileToJsonString = function (path) {
     return aux(sequences_array, '') ;
 }
 
-//à partir du path d'un fichier.fasta, le traduit en JSON séparant chaque séquence.
+/*
+ * fastaFileToJsonObject(path) : prend le chemin d'un fichier fasta et crée un objet JSON à partir de celui-ci.
+ */
 exports.fastaFileToJsonObject = function (path){
     return JSON.parse(exports.fastaFileToJsonString(path)) ;
 }
 
-//à partir du path1 d'un fichier.fasta, crée un fichier.json au path2 séparant chaque séquence.
+/*
+ * fastaFileToJsonFile(path1, path2) : convertit le fichier fasta d'adresse path1 en un fichier JSON d'adresse path2.
+ * renvoie une erreur en cas de problème d'écriture, affiche une confirmation sur la console sinon.
+*/
 exports.fastaFileToJsonFile = function(path1, path2){
     var json_string = exports.fastaFileToJsonString(path1) ;
     fs.writeFile(path2, json_string, function(err){
@@ -47,15 +72,24 @@ exports.fastaFileToJsonFile = function(path1, path2){
     }) ;
 }
 
-//à partir du path d'un fichier.json, retourne l'objet JSON.
+/*
+ * jsonFileToJsonObject(path) : importe le fichier JSON d'adresse path dans un objet JSON.
+ */
 exports.jsonFileToJsonObject = function (path) {
     var json_string = fs.readFileSync(path, 'ascii') ;
     return JSON.parse(json_string) ;
 }
 
+/*
+ * jsonObjectToFastaFile(json, path) : exporte un objet JSON en fichier fasta.
+ */
 exports.jsonObjectToFastaFile = function(json, path) {
     var res = '' ;
-    
+
+    /*
+     * fonction auxiliaire qui reformate l'id de la séquence au format fasta
+     * et le concatène à la chaîne de la séquence.
+     */
     var aux = function(json){
 	var id = '>'+json.id+'\n' ;
 	var seq =json.seq ;
